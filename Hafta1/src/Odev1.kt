d213a3d451cdb9058295d0cdbc1b5c4ae77db42a
Omer Bitikcioglu <o.bitikcioglu@gmail.com>
2022-01-13 14:03:51 +0300

    first commit

---
+++ Hafta1/src/Odev1.kt
@@ -0,0 +1,37 @@
+/**
+ * Innova Android Bootcamp
+ * Ödev - 1
+ *
+ * @author Ömer Faruk Bitikçioğlu
+ */
+
+fun main() {
+    // İşlem
+    var islemNo = 5
+    var ogrNo = 10
+    var kitapNo = 20
+    var aTarih = "08.01.2022"
+    var vTarih = "15.01.2022"
+
+    // Ogrenci
+    var ogrAd = "Ahmet"
+    var ogrSoyad = "Başaran"
+    var cinsiyet = "Erkek"
+    var dTarih = "16.01.1997"
+    var sinif = "11-B"
+
+    // Kitap
+    var isbnNo = "9789750739903"
+    var kitapAdi = "Aşk ve Gurur"
+    var yazarNo = 55
+    var turNo = 3
+    var sayfaSayisi = 456
+    var puan = 4.5
+
+    // Yazar
+    var yazarAd = "Jane"
+    var yazarSoyad = "Austen"
+
+    // Tür
+    var turAdi = "Roman"
+}
